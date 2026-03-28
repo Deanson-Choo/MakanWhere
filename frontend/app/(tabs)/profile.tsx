34004673458c9baf5ff32c84d3f1f882a0813e60
Deanson-Choo <deanson.choo@gmail.com>
2026-03-28 18:11:11 +0800

    [feat] added avatar to profile page Fixed #12

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -2,8 +2,9 @@ import { Text, StyleSheet, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { logout } from "@/services/auth";
 import useAuthStore from "@/store/authStore";
-import { useQueryClient, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { fetchReviewsByUser } from "@/services/reviews";
+import { Image } from 'expo-image'
 
 export default function Profile() {
     const { user } = useAuthStore();
@@ -15,13 +16,22 @@ export default function Profile() {
 
     return (
         <SafeAreaView style={styles.safeArea}>
-            <View>
-                <Text style={styles.welcomeText}>Welcome {user?.username}</Text>
+            <View style={{ alignItems: "center", marginTop: 32 }}>
+                <View style={{borderRadius: 50, overflow: "hidden", borderColor: "#d6e1f3", borderWidth: 2}}>
+                    <Image 
+                    source={{ uri: `https://api.dicebear.com/9.x/personas/svg?seed=${user?.id}` }}
+                    style={{ width: 100, height: 100 }} 
+                    contentFit="cover"
+                    />
+                </View>
+
+                <Text style={styles.welcomeText}>{user?.username}</Text>
                 <Text style={styles.reviewCount}>Reviews: {data?.length ?? 0}</Text>
+
+                <TouchableOpacity onPress={logout} style={styles.logoutButton}>
+                    <Text style={styles.logoutButtonText}>Log Out</Text>
+                </TouchableOpacity>
             </View>
-            <TouchableOpacity onPress={logout} style={styles.logoutButton}>
-                <Text style={styles.logoutButtonText}>Log Out</Text>
-            </TouchableOpacity>
         </SafeAreaView>
     );
 }
@@ -30,6 +40,7 @@ const styles = StyleSheet.create({
     safeArea: {
         flex: 1,
         backgroundColor: "#F3F4F6",
+        position: "relative"
     },
     logoutButton: {
         backgroundColor: "#EF4444",
@@ -37,6 +48,7 @@ const styles = StyleSheet.create({
         padding: 12,
         alignItems: "center",
         margin: 14,
+        width: "90%",
     },
     logoutButtonText: {
         color: "#FFFFFF",
